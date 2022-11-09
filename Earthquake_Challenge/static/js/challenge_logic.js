b31355b3a9419569fe2ccf5d653433496872acc3
gabischlegel <gabrielle.schlegel@utexas.edu>
2022-11-09 01:42:38 -0600

    Challenge Complete

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -123,17 +123,27 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson").then(function(data) {
 
   // 4. Use the same style as the earthquake data.
-  style: styleInfo,
-  
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.properties.mag),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
   // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
   function getColor(magnitude) {
-    if (magnitude > 5) {
+    if (magnitude > 6) {
       return "#ff0000";
-    }
-    if (magnitude > 4) {
-      return "#ffefd5";
-    }
-    return "#00ff7f";
+    } 
+    if (magnitude > 5) {
+      return "#ffff00";
+    } 
+    return "#adff2f";
+    
   }
   
   // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
@@ -154,9 +164,9 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
     },
     style: styleInfo,
     onEachFeature: function(feature, layer) {
-      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+      layer.bindPopup("<h3> Magnitude: " + feature.properties.mag + "</h3> <hr><h3> Location: " + feature.properties.place + "</h3>");
     }
-  }).addTo(majorEeathquakes);
+  }).addTo(majorEarthquakes);
   })
   // 8. Add the major earthquakes layer to the map.
   majorEarthquakes.addTo(map);
